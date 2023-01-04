f2da5346b9e79922ac4e49406efda4c377322224
Aladdin Alawi <aladdin.alawi@corelogic.com.au>
2023-01-05 06:54:16 +0800

    Added Dashboard Items

--- lib/constants.dart
+++ lib/constants.dart
@@ -9,6 +9,8 @@ const kGrayColor = Color(0xfff5f5f5);
 const kSelectedColor = Color(0xfffdecdf);//##FDECDF
 const kDarkTextColor = Color(0xff0a0a0a);
 const kLightTextColor = Color(0xff757575);
+const kAvatarGrayColor = Color(0xffc2c2c2); //#C2C2C2
+const kOrangeColor = Color(0xffff914d); //##FF914D
 
 const iconList = [
   FontAwesomeIcons.person,
--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Waggital',
       theme: ThemeData(
         primarySwatch: Colors.blue,
